c2d807359391d0d408dd5ac6e951d6acef3bb6de
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-07 21:10:26 +0300

    new session

---
+++ Admin/New_session.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="add-new-doctors-pop-up ">
+        <form action="">
+            <div class="pop-up-header">
+                <h2>Add New Session</h2>
+                <p id="x-sign">&times;</p>
+            </div>
+            <label for="name">Session Title</label><br>
+            <input type="text" name="" id="name" placeholder="Name of This Session"><br>
+            <label for="speciality">Select Doctor</label><br>
+            <select name=" " id="select" placeholder="Choose doctor name from list">
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select><br>
+            <label for="nic">Number of Patients/Appointment Numbers</label><br>
+            <input type="number" name="" id="nic" placeholder="The Finial Appointment Number"><br>
+            <label for="email">Session Date</label><br>
+            <input type="date" name="" id="email"><br>
+            <label for="telephone">Schedule Time</label><br>
+            <input type="time" name="" id="telephone"><br>
+            <input type="submit" value="Place This Session" id="add-button">
+            <input type="reset" id="rest-button">
+        </form>
+    </div>
+
+</body>
+
+</html>
