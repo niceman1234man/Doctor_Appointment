a6c1abd0a36c050e84c504a4bc009efb28c5f324
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-07 21:45:13 +0300

    update

--- Admin/New_session.php
+++ Admin/New_session.php
@@ -9,26 +9,26 @@
 
 <body>
     <div class="add-new-doctors-pop-up ">
-        <form action="">
+        <form action="" method="post">
             <div class="pop-up-header">
                 <h2>Add New Session</h2>
                 <p id="x-sign">&times;</p>
             </div>
             <label for="name">Session Title</label><br>
-            <input type="text" name="" id="name" placeholder="Name of This Session"><br>
+            <input type="text" name="title" id="name" placeholder="Name of This Session"><br>
             <label for="speciality">Select Doctor</label><br>
-            <select name=" " id="select" placeholder="Choose doctor name from list">
+            <select name=" name " id="select" placeholder="Choose doctor name from list">
                 <option value=""></option>
                 <option value=""></option>
                 <option value=""></option>
             </select><br>
             <label for="nic">Number of Patients/Appointment Numbers</label><br>
-            <input type="number" name="" id="nic" placeholder="The Finial Appointment Number"><br>
+            <input type="number" name="nic" id="nic" placeholder="The Finial Appointment Number"><br>
             <label for="email">Session Date</label><br>
-            <input type="date" name="" id="email"><br>
+            <input type="date" name="date" id="email"><br>
             <label for="telephone">Schedule Time</label><br>
-            <input type="time" name="" id="telephone"><br>
-            <input type="submit" value="Place This Session" id="add-button">
+            <input type="time" name="time" id="telephone"><br>
+            <input type="submit" value="Place This Session" id="add-button" name="submit">
             <input type="reset" id="rest-button">
         </form>
     </div>
@@ -36,3 +36,17 @@
 </body>
 
 </html>
+<?php
+
+if(isset($_POST["submit"])){
+    
+$title=$_POST["title"];
+$name=$_POST["name"];
+$nic=$_POST["nic"];
+$time=$_POST["time"];
+$speciality=$_POST["speciality"];
+
+
+}
+
+?>
