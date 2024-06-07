67f3c7d2d1be74b898bbb83c89813571a89fb396
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-07 20:48:19 +0300

    added

---
+++ Admin/add_new_doctor.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="add-new-doctors-pop-up ">
+        <form action="">
+            <div class="pop-up-header">
+                <h2>Add New Doctor</h2>
+                <p id="x-sign">&times;</p>
+            </div>
+            <label for="name">Name</label><br>
+            <input type="text" name="" id="name" placeholder="Name Doctor"><br>
+            <label for="email">Email</label><br>
+            <input type="email" name="" id="email" placeholder="Email Address "><br>
+            <label for="nic">NIC</label><br>
+            <input type="number" name="" id="nic" placeholder="NIC Number "><br>
+            <label for="telephone">Telephone</label><br>
+            <input type="number" name="" id="telephone" placeholder="Telephone Number "><br>
+            <label for="speciality">Speciality</label><br>
+            <select name=" " id="select">
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select><br>
+            <label for="password">specialityPassword</label><br>
+            <input type="text" name="" id="password" placeholder="Enter Password "><br>
+            <label for="confirm">Confirm Password</label><br>
+            <input type="password" name="" id="confirm" placeholder="Confirm Password"><br>
+            <input type="submit" value="Add" id="add-button">
+            <input type="reset" id="rest-button">
+        </form>
+    </div>
+</body>
+
+</html>
