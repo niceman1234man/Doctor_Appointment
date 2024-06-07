f3d260be1e1271e3ee4374ff4f6e09e34e096881
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-07 21:29:19 +0300

    update

--- Admin/add_new_doctor.php
+++ Admin/add_new_doctor.php
@@ -15,27 +15,44 @@
                 <p id="x-sign">&times;</p>
             </div>
             <label for="name">Name</label><br>
-            <input type="text" name="" id="name" placeholder="Name Doctor"><br>
+            <input type="text" name="name" id="name" placeholder="Name Doctor"><br>
             <label for="email">Email</label><br>
-            <input type="email" name="" id="email" placeholder="Email Address "><br>
+            <input type="email" name="email" id="email" placeholder="Email Address "><br>
             <label for="nic">NIC</label><br>
-            <input type="number" name="" id="nic" placeholder="NIC Number "><br>
+            <input type="number" name="nic" id="nic" placeholder="NIC Number "><br>
             <label for="telephone">Telephone</label><br>
-            <input type="number" name="" id="telephone" placeholder="Telephone Number "><br>
+            <input type="number" name="telephone" id="telephone" placeholder="Telephone Number "><br>
             <label for="speciality">Speciality</label><br>
-            <select name=" " id="select">
+            <select name=" speciality" id="select">
                 <option value=""></option>
                 <option value=""></option>
                 <option value=""></option>
             </select><br>
             <label for="password">specialityPassword</label><br>
-            <input type="text" name="" id="password" placeholder="Enter Password "><br>
+            <input type="text" name="password" id="password" placeholder="Enter Password "><br>
             <label for="confirm">Confirm Password</label><br>
-            <input type="password" name="" id="confirm" placeholder="Confirm Password"><br>
-            <input type="submit" value="Add" id="add-button">
+            <input type="password" name="confirm" id="confirm" placeholder="Confirm Password"><br>
+            <input type="submit" value="Add" id="add-button" name="submit">
             <input type="reset" id="rest-button">
         </form>
     </div>
 </body>
 
 </html>
+
+
+<?php
+if(isset($_POST["submit"])){
+    
+$name=$_POST["name"];
+$email=$_POST["eamil"];
+$nic=$_POST["nic"];
+$telephone=$_POST["telephone"];
+$speciality=$_POST["speciality"];
+$password=$_POST["password"];
+$confirm=$_POST["confirm"];
+
+}
+
+
+?>
