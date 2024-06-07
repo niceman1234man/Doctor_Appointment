2e6a356a2cd3e717b5590d27d065af82a2ff2e85
mengaww2ya <mengaww2ya@gmail.com>
2024-06-07 22:25:05 +0300

    pop up seting part

--- js/index.js
+++ js/index.js
@@ -7,30 +7,46 @@ function date() {
   document.querySelector('.todaysDate').innerHTML += ` ${day}/${month}/${year}`;
 }
 date();
+const disp1=displaySetting();
+const disp2=displayDetail();
+const disp3=displayDelete();
+const backPage=document.getElementById('myApointment');
+function  bluredBackPage(){
+  backPage.style.left = '0';
+  backPage.style.width = '100%';
+  backPage.style.height = '100%';
+  backPage.style.zIndex = '9999';
+  backPage.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+}
 function displaySetting() {
-  document.querySelector('.accountSetting').classList.add('active');
+  document.querySelector('.accountSetting').classList.add("active");
 }
 function hidSetting() {
-  document.querySelector('.accountSetting').classList.add('remove');
+  document.querySelector('.accountSetting').classList.remove("active");
+  
 }
 
-date();
 function displayDetail() {
-  document.querySelector('.viewdetail').classList.add('active');
+  document.querySelector('.viewdetail').classList.add("active");
 }
 function hidDetail() {
-  document.querySelector('.viewdetail').classList.add('remove');
+  document.querySelector('.viewdetail').classList.remove("active");
+  
 }
 function displayDelete() {
-  document.querySelector('.confiDelet').classList.add('active');
+  document.querySelector('.confiDelet').classList.add("active");
+
 }
 function hidDelete() {
-  document.querySelector('.confiDelet').classList.add('remove');
+  document.querySelector('.confiDelet').classList.remove("active");
+  
 }
-
-displaySetting();
-hidSetting();
-displayDetail();
+while(disp1()||disp2()||disp3()){
+  backPage();
+}
 hidDetail();
 displayDelete();
 hidDelete();
+
+
+
