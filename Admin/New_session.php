dd714eda52b56c268a00db1619b3a144c019cebe
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-08 15:58:57 +0300

    update

--- Admin/New_session.php
+++ Admin/New_session.php
@@ -9,7 +9,7 @@
 
 <body>
     <div class="add-new-doctors-pop-up ">
-        <form action="" method="post">
+        <form action="New_session.php" method="post">
             <div class="pop-up-header">
                 <h2>Add New Session</h2>
                 <p id="x-sign">&times;</p>
@@ -17,10 +17,10 @@
             <label for="name">Session Title</label><br>
             <input type="text" name="title" id="name" placeholder="Name of This Session"><br>
             <label for="speciality">Select Doctor</label><br>
-            <select name=" name " id="select" placeholder="Choose doctor name from list">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+            <select name="speciality" id="select" placeholder="Choose doctor name from list">
+                <option value="abebe">abeb</option>
+                <option value="degu">adu</option>
+                <option value="baby"> bela</option>
             </select><br>
             <label for="nic">Number of Patients/Appointment Numbers</label><br>
             <input type="number" name="nic" id="nic" placeholder="The Finial Appointment Number"><br>
@@ -41,11 +41,22 @@
 if(isset($_POST["submit"])){
     
 $title=$_POST["title"];
-$name=$_POST["name"];
+$date=$_POST["date"];
 $nic=$_POST["nic"];
 $time=$_POST["time"];
-$speciality=$_POST["speciality"];
+// $speciality=$_POST["speciality"];
 
+// echo $title . "<br>";
+// echo $date . "<br>";
+// echo $nic . "<br>";
+// if (isset($_POST["speciality"])) {
+//     $speciality = $_POST["speciality"];
+//     // Process the selected speciality
+//     echo "Selected speciality: " . $speciality;
+// } else {
+//     echo "No speciality selected.";
+// }
+// echo $time . "<br>";
 
 }
 
