f3548405ef850dcd766db8875b919f9919408f76
mengaww2ya <mengaww2ya@gmail.com>
2024-06-10 22:26:08 +0300

    i will not forget this time to correct one form i take 2 dayes without getting errors but todays i can fix (connect php and xamp server and work correctly )

--- mengphp/addDoctor.php
+++ mengphp/addDoctor.php
@@ -1,24 +1,31 @@
 <?php
+// Include the database connection file
 include("connection.php");
-if(isset($_POST["submit"])){
-$fName=$_POST["Fname"];
-$lName=$_POST["Lname"];
-$email=$_POST["email"];
-$nic=$_POST["NIC"];
-$telephone=$_POST["Telephone"];
-$speciality=$_POST["specialties"];
-$password=$_POST["password"];
-$confirm=$_POST["ConformPassword"];
-$sql="insert into   Doctor  values('$fName','$lName','$nic','$email','$telephone','$speciality','$password')";
-if($password==$confirm){
-    mysqli_query($conn,$sql);
-    echo "New Doctor Added!";
-}else{
-    echo "please insert correct password!";
+
+if (isset($_POST["submit"])) {
+    $fName = $_POST["Fname"];
+    $lName = $_POST["Lname"];
+    $nic = $_POST["NIC"];
+    $email = $_POST["email"];
+    $telephone = $_POST["Telephone"];
+    $speciality = $_POST["specialties"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["ConformPassword"];
+
+    if ($password !== $confirmPassword) {
+        echo "Passwords do not match.";
+        return;
+    }
+    $sql = "INSERT INTO Doctor VALUES ('$fName', '$lName', '$nic', '$email', '$telephone', '$speciality', '$password')";
+
+    if (mysqli_query($connection, $sql)) {
+        echo "New Doctor Added!";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+    }
+} else {
+    echo "No form data submitted.";
 }
 
-
-}
-
-mysqli_close($conn);
+mysqli_close($connection);
 ?>
--- mengphp/connection.php
+++ mengphp/connection.php
@@ -9,7 +9,8 @@ $connection = new mysqli($serverName, $userName, $password, $dbName);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
+else{
 echo "Connected successfully";
+}
 
-$connection->close();
 ?>
