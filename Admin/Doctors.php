45691b5879076dcb3dd6fd62470db2bc3b83f4ba
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-12 00:09:09 +0300

    updated doctor

--- Admin/Doctors.php
+++ Admin/Doctors.php
@@ -14,14 +14,14 @@
     <?php
   include("sidebar.php");
   ?>
-
     <div class="main-part">
         <img src="../img/search.svg" alt="search" id="search-img">
         <input type="search" placeholder="search Doctor Name or Email" id="search">
-
         <button id="search-button">search</button>
-        <p id="today-date">Today's date <img src="../img/calendar.svg" alt=""><br></p><br>
-
+        <p id="today-date">Today's date <img src="../img/calendar.svg" alt=""><br>
+            <?php date_default_timezone_set('Asia/Kolkata'); 
+            $today=date('Y-m-d');
+             echo $today; ?></p><br>
         <div class="add-new-section">
             <h2>Add new Doctor</h2>
             <button id="add-new-button">+ Add New</button>
@@ -32,6 +32,14 @@
         $result = mysqli_query($conn, $list1);
         ?>
         <p>All Doctors (<?php echo mysqli_num_rows($result); ?>)</p>
+        <?php
+    if(isset($_POST["submit"])) {
+        echo "<div class='new-doctor-added-popup'>
+            <h3>New Doctor Added!</h3>
+            <p>You will be redirected to the Doctors page in a few seconds.</p>
+        </div>";
+    }
+    ?>
         <table>
             <tr>
                 <th>Doctor Name</th>
@@ -61,12 +69,38 @@ if(mysqli_num_rows($result) > 0) {
 }
 ?>
         </table>
-
+        <div class="add-new-doctors-pop-up ">
+            <form action="add_new_doctor.php" method="post">
+                <div class="pop-up-header">
+                    <h2>Add New Doctor</h2>
+                    <p id="x-sign">&times;</p>
+                </div>
+                <label for="name">Name</label><br>
+                <input type="text" name="name" id="name" placeholder="Name Doctor"><br>
+                <label for="email">Email</label><br>
+                <input type="email" name="email" id="email" placeholder="Email Address "><br>
+                <label for="nic">NIC</label><br>
+                <input type="number" name="nic" id="nic" placeholder="NIC Number "><br>
+                <label for="telephone">Telephone</label><br>
+                <input type="number" name="telephone" id="telephone" placeholder="Telephone Number "><br>
+                <label for="speciality">Speciality</label><br>
+                <select name=" speciality" id="select">
+                    <option value="nurse">nurse</option>
+                    <option value="anstesia">Anstesia</option>
+                    <option value="padio">Padio</option>
+                </select><br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password" id="password" placeholder="Enter Password "><br>
+                <label for="confirm">Confirm Password</label><br>
+                <input type="password" name="confirm" id="confirm" placeholder="Confirm Password"><br>
+                <input type="submit" value="Add" id="add-button" name="submit">
+                <input type="reset" id="rest-button">
+            </form>
+        </div>
         <?php
-        
-        include("add_new_doctor.php");
         include("connection.php");
         ?>
+        <p class="view"></p>
         <div class="doctor-detail-pop-up">
             <div class="pop-up-header">
                 <h2>View Detail</h2>
@@ -84,9 +118,10 @@ if(mysqli_num_rows($result) > 0) {
             <p></p>
             <button>ok</button>
         </div>
-
     </div>
-    <script src="..JS/index.js"></script>
+    <!-- <script src="..JS/index.js"></script> -->
+    <!-- <script src="../JS/ab.js"></script> -->
+    <script src="../JS/index.js"></script>
 </body>
 
 </html>
