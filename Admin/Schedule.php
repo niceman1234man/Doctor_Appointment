0b3001fc1a88a0c69a3c2927a4b0d4d9bef7eed1
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-12 00:08:14 +0300

    updated schedule

--- Admin/Schedule.php
+++ Admin/Schedule.php
@@ -71,9 +71,30 @@ if(mysqli_num_rows($result) > 0) {
 }
 ?>
         </table>
-        <?php
-        include("New_session.php");
-         ?>
+        <div class="add-new-doctors-pop-up">
+            <form action="New_session.php" method="post">
+                <div class="pop-up-header">
+                    <h2>Add New Session</h2>
+                    <p id="x-sign">&times;</p>
+                </div>
+                <label for="name">Session Title</label><br>
+                <input type="text" name="title" id="name" placeholder="Name of This Session"><br>
+                <label for="speciality">Select Doctor</label><br>
+                <select name="speciality" id="select" placeholder="Choose doctor name from list">
+                    <option value="abebe">abeb</option>
+                    <option value="degu">adu</option>
+                    <option value="baby"> bela</option>
+                </select><br>
+                <label for="nic">Number of Patients/Appointment Numbers</label><br>
+                <input type="number" name="num" id="nic" placeholder="The Finial Appointment Number"><br>
+                <label for="email">Session Date</label><br>
+                <input type="date" name="date" id="email"><br>
+                <label for="telephone">Schedule Time</label><br>
+                <input type="time" name="time" id="telephone"><br>
+                <input type="submit" value="Place This Session" id="add-button" name="submit">
+                <input type="reset" id="rest-button">
+            </form>
+        </div>
         <div class="schedule-detail-pop-up">
             <div class="pop-up-header">
                 <h2>View Detail</h2>
