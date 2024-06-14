cd01094d2eadd906d6ff1fee8b66e96eec5bab3d
mengaww2ya <mengaww2ya@gmail.com>
2024-06-14 14:24:52 +0300

    resolving conflict

--- Admin/Patients.php
+++ Admin/Patients.php
@@ -11,38 +11,15 @@
 
 <body>
     <?php
-    include("connection.php");
-    $list3 = "select * from patient";
-       $result = mysqli_query($conn, $list3);
     include("sidebar.php");
     ?>
     <div class="main-part">
-        <form action="Patients.php" method="post">
-            <img src="../img/search.svg" alt="search" id="search-img">
-            <input type="search" placeholder="search Patient Name or Email" id="search" name="search_term">
-            <button type="submit" name="search" id="search-button">search</button>
-        </form>
-        <p id="today-date">Today's date <img src="../img/calendar.svg" alt=""><br>
-            <?php date_default_timezone_set('Asia/Kolkata');
+        <img src="../img/search.svg" alt="search" id="search-img">
+        <input type="search" placeholder="search Patient Name or Email" id="search">
+        <button id="search-button">search</button>
+        <p id="today-date">Today's date <img src="../img/calendar.svg" alt=""></p>
 
-            $today = date('Y-m-d');
-            echo $today;?>
-        </p>
-
-        <p>All Patients( <?php echo mysqli_num_rows($result);?>)</p>
-
-        <?php
-           if (isset($_POST['search'])) {
-            $search_term = $_POST['search_term'];
-            $list1 = "SELECT * FROM patient WHERE name LIKE '%$search_term%' OR email LIKE '%$search_term%'";
-            $result = mysqli_query($conn, $list1);
-        } else {
-            $list1 = "SELECT * FROM patient";
-            $result = mysqli_query($conn, $list1);
-        }
-        
-        
-        echo '
+        <p>All Patients(1)</p>
         <table>
             <tr>
                 <th> Name</th>
@@ -51,39 +28,53 @@
                 <th>Email</th>
                 <th>Date of birth</th>
                 <th>Events</th>
-            </tr>';
-          
-     
+            </tr>
+            <?php
+     include("connection.php");
+     $list2 = "select * from session";
+        $result = mysqli_query($conn, $list2);
 if(mysqli_num_rows($result) > 0) {
     $data = '';
     while($row = mysqli_fetch_assoc($result)) {
-        $name = $row["name"];
-        $nic = $row["nic"];
-        $tel = $row["telephone"];
-        $email = $row["email"];
-        $date_of_birth = $row["date_of_birth"];
-        $id=$row["id"];
+        $title = $row["title"];
+        $spec = $row["dname"];
+        $mnum = $row["num"];
+        $d = $row["date"];
+        $t = $row["time"];
+      
         $data .= '<tr>
-            <td>' .  $name . '</td>
-            <td>' .   $nic  . '</td>
-            <td>' .  $tel .'</td>
-            <td>' . $email. '</td>
-             <td>' . $date_of_birth . '</td>
+            <td>' . $title . '</td>
+            <td>' . $spec . '</td>
+            <td>' .  $d . " ".  $t .'</td>
+            <td>' . $mnum . '</td>
             <td>
-          <form action="view_patient.php" method="post" style="display:flex;">
-    <input type="hidden" name="id" value="'. $id .'">
-    <button type="submit" class="view-button">
-        <img src="../img/icons/view-iceblue.svg" alt="View" value="View">View
-    </button>
-</form>
-          </td>
-          </tr>';
+                <button class="view-button"><img src="../img/icons/view-iceblue.svg" alt="">View</button>
+                <button><img src="../img/icons/delete-iceblue.svg" alt="">Remove</button>
+            </td>
+        </tr>';
     }
     echo $data;
 }
 ?>
         </table>
-
+        <div class="patients-detail-pop-up">
+            <div class="pop-up-header">
+                <h2>View Detail</h2>
+                <p id="x-sign">&times;</p>
+            </div>
+            <p>Patient ID</p>
+            <p></p>
+            <p>Name :</p>
+            <p></p>
+            <p>Email :</p>
+            <p></p>
+            <p> NIC :</p>
+            <p></p>
+            <p>Telephone :</p>
+            <p></p>
+            <p>Address :</p>
+            <p></p>
+        </div>
         <script src="../JS/index.js"></script>
 </body>
 
--- loginpage/signup.php
+++ loginpage/signup.php
@@ -1,60 +1,98 @@
-<!-- <?php
+<?php>
 
-session_start();
+// Include config file
+require "Connection.php";
 
-$_SESSION["user"]="";
-$_SESSION["usertype"]="";
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
 
-// Set the new timezone
-date_default_timezone_set('Asia/Kolkata');
-$date = date('Y-m-d');
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-$_SESSION["date"]=$date;
-//import database
-include("connection.php");
-if($_POST){
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
 
-    $result= $database->query("select * from webuser");
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-    $fname=$_SESSION['personal']['fname'];
-    $lname=$_SESSION['personal']['lname'];
-    $name=$fname." ".$lname;
-    $address=$_SESSION['personal']['address'];
-    $nic=$_SESSION['personal']['nic'];
-    $dob=$_SESSION['personal']['dob'];
-    $email=$_POST['newemail'];
-    $tele=$_POST['tele'];
-    $newpassword=$_POST['newpassword'];
-    $cpassword=$_POST['cpassword'];
-    
-    if ($newpassword==$cpassword){
-        $result= $database->query("select * from webuser where email='$email';");
-        if($result->num_rows==1){
-            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Already have an account for this Email address.</label>';
-        }else{
-            
-            $database->query("insert into patient(pemail,pname,ppassword, paddress, pnic,pdob,ptel) values('$email','$name','$newpassword','$address','$nic','$dob','$tele');");
-            $database->query("insert into webuser values('$email','p')");
+            // Set parameters
+            $param_username = trim($_POST["username"]);
 
-            //print_r("insert into patient values($pid,'$email','$fname','$lname','$newpassword','$address','$nic','$dob','$tele');");
-            $_SESSION["user"]=$email;
-            $_SESSION["usertype"]="p";
-            $_SESSION["username"]=$fname;
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
 
-            header('Location: patient/index.php');
-            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
-        
-    }else{
-        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Password Conformation Error! Reconform Password</label>';
     }
 
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
 
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
 
-    
-}else{
-        //header('location: signup.php');
-    $error='<label for="promter" class="form-label"></label>';
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($link);
 }
 
-?> -->
+<?
