4d45f57695faf11bb6009ff9b1c498892c7c56b4
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-14 13:20:52 +0300

    updated schedue

--- Admin/Schedule.php
+++ Admin/Schedule.php
@@ -31,24 +31,25 @@
         </div>
         <p>All Sessions( <?php echo mysqli_num_rows($result)?>)</p>
         <div class="input-sections">
-            Date:<input type="date" name="date" id="date">
-            Doctor: <select name="doctor" id="doctor">
-                <?php
+            <form action="">
+                Date:<input type="date" name="date" id="date">
+                Doctor: <select name="doctor" id="doctor">
+                    <?php
              while($row=mysqli_fetch_assoc($resultd)){
                 $data2 =$row["name"] ;
                 echo '
                 <option value="">'. $data2 . '</option>
                 ';
-
             }
             ?>
-            </select>
+                </select>
 
-
-
-            <button><img src="../img/icons/filter-iceblue.svg" alt="" id="filter"> Filter</button>
-
+                <button type="submit" value="Filter" name="filter"><img src="../img/icons/filter-iceblue.svg" alt=""
+                        id="filter">Filter
+                </button>
+            </form>
         </div>
+        <?php echo '
         <table>
             <tr>
                 <th>Session Title</th>
@@ -56,46 +57,63 @@
                 <th>Schedule date&time</th>
                 <th>Max num Booked</th>
                 <th>Events</th>
-            </tr>
-            <?php
-     
-if(mysqli_num_rows($result) > 0) {
-    $data = '';
-    while($row = mysqli_fetch_assoc($result)) {
+            </tr>';
+           
+            if(isset($_POST["filter"])){
+            if (isset($_POST['search_title']) && isset($_POST['search_date'])) {
+                $search_title = $_POST['search_title'];
+                $search_date = $_POST['search_date'];
+            
+                // Construct the SQL query to search for sessions
+                $list2 = "SELECT * FROM session WHERE title LIKE '%$search_title%' AND date = '$search_date'";
+                $result = mysqli_query($conn, $list2);
+            }else{
+                echo 'please inset all ';
+            } }
+            else {
+                // If no search criteria are provided, fetch all the sessions
+                $list2 = "SELECT * FROM session";
+                $result = mysqli_query($conn, $list2);
+            }
+            
+        if(mysqli_num_rows($result) > 0) {
+        $data = '';
+        while($row = mysqli_fetch_assoc($result)) {
         $title = $row["title"];
-        $spec = $row["dname"]; 
+        $spec = $row["dname"];
         $mnum = $row["num"];
         $d = $row["date"];
         $t = $row["time"];
         $id=$row["id"];
-      
+
         $data .= '<tr>
             <td>' . $title . '</td>
             <td>' . $spec . '</td>
-            <td>' .  $d . " ".  $t .'</td>
+            <td>' . $d . " ". $t .'</td>
             <td>' . $mnum . '</td>
             <td>
-            <div class="schedule-form-button">
-        <form action="viewSchedule.php" method="post" style="display:flex;">
-          <input type="hidden" value="' . $id .'">
-         <button type="submit" class="view-button">
-        <img src="../img/icons/view-iceblue.svg" alt="View" value="View">View
-          </button>
-       </form>
-
-    <form action="deleteSchedule.php" method="post">
-    <input type="hidden" value="' . $id .'">
-    <button type="submit">
-        <img src="../img/icons/delete-iceblue.svg" alt="Remove" value="Remove">
-    </button>
-</form>
- </div>
-            </td>
-        </tr>';
+                <div class="schedule-form-button">
+                    <form action="viewSchedule.php" method="post" style="display:flex;marigin-right: 5px;">
+                        <input type="hidden" name="id" value="'. $id .'">
+                        <button type="submit" class="view-button">
+                            <img src="../img/icons/view-iceblue.svg" alt="View" value="View">View
+                        </button>
+                    </form>
+                    <div class="schedule-form-button">
+            <form action="deleteSchedule.php" method="post" style="display:flex;">
+        <input type="hidden" name="id" value="'. $id .'">
+        <button type="submit" class="delete-button">
+            <img src="../img/icons/delete-iceblue.svg" alt="Remove" value="Remove">
+        </button>
+        </form>
+    </div>
+    </div>
+    </td>
+    </tr>';
     }
     echo $data;
-}
-?>
+    }
+    ?>
         </table>
         <div class="add-new-doctors-pop-up">
             <form action="New_session.php" method="post">
