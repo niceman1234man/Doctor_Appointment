da72491e77fc54aa797f6032a5db87dbcacf7df4
mengaww2ya <mengaww2ya@gmail.com>
2024-06-14 23:54:29 +0300

    mengphp/mypationt.php

--- mengphp/mypationt.php
+++ mengphp/mypationt.php
@@ -27,9 +27,55 @@
                 </select> <button class="filterbtn">
                     <img src="../images/icons/filter-iceblue.svg" class="filteimg">Filter</button></div>
             <div class="apointTable">
-                <table class="table" cellspacing="50"><tr><th> name	</th><th>NIC</th><th>Telephone	</th><th>Email</th><th>Date of Birth</th><th>Events</th></tr>
-            <tr><td> Test Patient	</td><td>0000000000	</td><td>0120000000</td><td>patient@edoc.com	</td><td>2000-01-01	
-            </td><td><button class="tablebtn" onclick="displayPationtDetail()"><img src="../images/icons/view-iceblue.svg" > view</button ></td></tr></table>
+            <?php
+include("connection.php");
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = "SELECT *FROM `Patients` WHERE 1";
+$result = $connection->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>First Name</th>";
+    echo "<th>Last Name</th>";
+    echo "<th>NIC</th>";
+    echo "<th>Telephone</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date of Birth</th>";
+    echo "<th>ID</th>";
+    echo "<th>Actions</th>";
+    echo "</tr>";
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["Fname"] . "</td>";
+        echo "<td>" . $row["Lname"] . "</td>";
+        echo "<td>" . $row["NIC"] . "</td>";
+        echo "<td>" . $row["Telephone"] . "</td>";
+        echo "<td>" . $row["Email"] . "</td>";
+        echo "<td>" . $row["Date of Birth"] . "</td>";
+        echo "<td>" . $row["ID"] . "</td>";
+        echo "<td>
+            <div class=\"form-button\">
+                <form action=\"view_patient.php\" method=\"post\">
+                    <input type=\"hidden\" name=\"id\" value=\"" . $row["ID"] . "\">
+                    <button type=\"submit\" class=\"viewbutton\" onclick=\"displayPationtDetail();\"  >
+                        <img src=\"../images/icons/view-iceblue.svg\" alt=\"View\" value=\"View\">View
+                    </button>
+                </form>
+            </div>
+        </td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No results found.";
+}
+$connection->close();
+?>
               </div>
             </div>
             </section>
