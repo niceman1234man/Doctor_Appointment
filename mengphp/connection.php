70693913a1ce5304b0a554c5fc736ad8341fdd45
mengaww2ya <mengaww2ya@gmail.com>
2024-06-14 21:01:44 +0300

    mengphp/connection.php

--- mengphp/connection.php
+++ mengphp/connection.php
@@ -9,8 +9,5 @@ $connection = new mysqli($serverName, $userName, $password, $dbName);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
-else{
-echo "Connected successfully";
-}
 
 ?>
