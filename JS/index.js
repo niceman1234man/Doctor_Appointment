0ef921894a3fc08aea44cb8f43751caa54379854
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-16 09:26:45 +0300

    updated

--- JS/index.js
+++ JS/index.js
@@ -7,55 +7,25 @@ document.getElementById('add-new-button').onclick = () => {
 document.getElementById('add-button').addEventListener('click', (event) => {
   event.preventDefault(); // Prevent the default form submission
 
-  // Gather the form data
-  const formData = new FormData(document.querySelector('form'));
-
-  // Use AJAX to send the form data to the server
-  fetch('New_session.php', {
-    method: 'POST',
-    body: formData,
-  })
-    .then((response) => {
-      // Handle the response from the server
-      if (response.ok) {
-        // Display a success message or update the page
-
-        // Hide the popup
+  
         document.querySelector('.add-new-doctors-pop-up').classList.remove('active');
-      } else {
-        // Display an error message
-
-      }
-    });
-});
-// document.querySelector('#add-new-button').addEventListener('click', () => {
-
-// });
+      
+       
 document.querySelector('#x-sign').addEventListener('click', () => {
   document.querySelector('.add-new-doctors-pop-up').classList.remove('active');
 });
 
-// document.querySelector('#today-date').appendChild(datee);
-document.getElementById('add-button').addEventListener('click', () => {
-  document.getElementById('name').focus();
-});
 
 document.querySelector('.view-button').addEventListener('click', () => {
   document.querySelector('.schedule-detail-pop-up').classList.add('active');
 });
 
 
-// Get all the "View" buttons
-
-// Add a click event listener to each "View" button
 
 
 
 
 
-// Get all the "View" buttons
-
-// Add a click event listener to each "View" button
 
 
 
@@ -77,4 +47,4 @@ document.querySelector('#delete').onclick=function confirmDelete(id) {
         document.body.appendChild(form);
         form.submit();
     }
-};
+}});
