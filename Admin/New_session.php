38447b3d61715834ae1d5fa1f71b68392f367da8
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-16 09:23:16 +0300

    updated

--- Admin/New_session.php
+++ Admin/New_session.php
@@ -1,4 +1,6 @@
 <?php
+$err="";
+$message="";
 include("../connection.php");
 if(isset($_POST["submit"])){
 $title=$_POST["title"];
@@ -6,12 +8,29 @@ $date=$_POST["date"];
 $num=$_POST["num"];
 $time=$_POST["time"];
 $speciality=$_POST["speciality"];
-$sql="insert into session  values('$title','$speciality','$num','$date','$time')";
-if(mysqli_query($conn,$sql)){
-    echo "New Session Added!";
-    header("Location:Schedule.php");
-}else{
-    echo "please insert correct password!";
+
+$sql = "INSERT INTO session (title, dname, num, date, time) 
+VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssss", $title, $speciality, $num, $date,$time);
+
+if ($stmt->execute()) {
+$message= "New Session Added!";
+} else {
+$err= "Error: " . $stmt->error;
 }
+
+echo '<center>
+      <div style="height: 30%; width: 20%; position:
+       absolute; top: 30%; 
+      left: 40%;border: 1px solid black;
+      border-radius: 5px;
+      ">
+       <h1>'.   $message . $err .'</h1> 
+       <button  style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;">  <a href="Schedule.php">CLOSE</a></button>
+     
+    </div>
+
+    </center>';
 }
 ?>
