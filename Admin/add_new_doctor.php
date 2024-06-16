31eb09ccce72757b6c55201411711280316c6274
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-16 09:24:00 +0300

    updated

--- Admin/add_new_doctor.php
+++ Admin/add_new_doctor.php
@@ -1,27 +1,44 @@
-<?php if(isset($_POST["submit"])){
-$name = $_POST["name"];
-$email = $_POST["email"];
-$nic = $_POST["nic"];
-$telephone = $_POST["telephone"];
-$speciality = $_POST["speciality"];
-$password = $_POST["password"];
-$confirm = $_POST["confirm"];
+<?php 
+include("../connection.php");
 
-if($password == $confirm){
-$sql = "INSERT INTO doctor (name, email, nic, telephone, speciality, password) VALUES (?, ?, ?, ?, ?, ?)";
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "ssssss", $name, $email, $nic, $telephone, $speciality, $password);
+if (isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $nic = $_POST["nic"];
+    $telephone = $_POST["telephone"];
+    $speciality = $_POST["speciality"];
+    $password = $_POST["password"];
+    $confirm = $_POST["confirm"];
+    $message="";
+    $err="";
 
-if(mysqli_stmt_execute($stmt)){
-echo "New Doctor Added!";
-header("location: Doctor.php");
-exit();
-} else {
-echo "Error: " . mysqli_stmt_error($stmt);
-}
+    if ($password == $confirm) {
+        $sql = "INSERT INTO doctor (name, email, nic, telephone, speciality, password) 
+                VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssss", $name, $email, $nic, $telephone, $speciality, $password);
 
-mysqli_stmt_close($stmt);
-} else {
-echo "Please insert correct password!";
+        if ($stmt->execute()) {
+            $message= "New Doctor Added!";
+        } else {
+            $err= "Error: " . $stmt->error;
+        }
+    } else {
+        $err= "Please insert correct password!";
+    }
+
+   echo '<center>
+      <div style="height: 20%; width: 20%; position:
+       absolute; top: 30%; 
+      left: 40%;border: 1px solid black;
+      border-radius: 5px;
+      ">
+       <h1>'.   $message . $err .'</h1> 
+       <button  style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;">  <a href="Doctors.php">CLOSE</a></button>
+     
+    </div>
+
+    </center>';
+    
 }
-}
+?>
