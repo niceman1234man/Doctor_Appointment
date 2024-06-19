f12c0ea6e0c5a8c032a7d94ebd98e3390d7cbbca
niceman1234man <yihunietarekegn18@gmail.com>
2024-06-19 08:09:11 +0300

    modified

--- Admin/deleteAppointment.php
+++ Admin/deleteAppointment.php
@@ -1,30 +1,29 @@
 <?php
 include("../connection.php");
-
 $message = "";
 $err = "";
 
-if(isset($_POST['id'])){
+if (isset($_POST['id'])) {
     $id = $_POST['id'];
 
     // Add confirmation dialog
     if(isset($_POST['confirm'])) {
-        $sql = "DELETE FROM doctor WHERE id='$id'";
-        if (mysqli_query($conn, $sql)) {
-            $message= "Doctor deleted successfully !!";
+        $delete_query = "DELETE FROM appointment WHERE id = '$id'";
+        if (mysqli_query($conn, $delete_query)) {
+            $message = "Appointment deleted successfully";
         } else {
-            $err= "Error deleting record: " . mysqli_error($conn);
+            $err = "Error deleting session: " . mysqli_error($conn);
         }
     } else {
         // Display confirmation dialog
         echo '<center>
         <div style="height: 40%; width: 20%; position: absolute; top: 30%; left: 40%;border: 1px solid black;border-radius: 5px;">
-            <h1>Are you sure you want to delete this doctor?</h1>
+            <h1>Are you sure you want to delete this session?</h1>
             <form method="post">
                 <input type="hidden" name="id" value="'.$id.'">
                 <input type="hidden" name="confirm" value="1">
                 <button style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;">Confirm</button>
-                <a href="Doctors.php" style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;">Cancel</a>
+                <a href="Appointment.php" style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;">Cancel</a>
             </form>
         </div>
         </center>';
@@ -32,12 +31,12 @@ if(isset($_POST['id'])){
     }
 
     echo '<center>
-    <div style="height: 30%; width: 20%; position: absolute; top: 30%; left: 40%;border: 1px solid black;border-radius: 5px;">
+    <div style="height: 40%; width: 30%; position: absolute; top: 30%; left: 40%;border: 1px solid black;border-radius: 5px;">
         <h1>'.$message.$err.'</h1>
-        <button style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;"><a href="Doctors.php">CLOSE</a></button>
+        <button style="text-decoration-style: none; padding: 2%; border-radius: 5%;background-color: rgb(102, 102, 156); color: white;"><a href="Appointment.php">CLOSE</a></button>
     </div>
     </center>';
-
-    mysqli_close($conn);
+} else {
+    echo "Session ID not provided.";
 }
 ?>
